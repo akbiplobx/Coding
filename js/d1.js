b2258defe924b2bbecfa1cd176f1d1af042d7e47
A K Biplob <akbiplobx@gmail.com>
2025-08-15 22:18:56 +0600

    d1-coding

---
+++ js/d1.js
@@ -0,0 +1,11 @@
+function makeNegative(num) {
+if (num > 0){
+  return num * (-1)
+} else{
+  return num
+}
+}
+
+console.log(makeNegative(5));  
+console.log(makeNegative(-3));  
+console.log(makeNegative(0)); 
