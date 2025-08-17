ea84c2dc4fe5af4a02ed9f0a01ffc2a97a2b4d52
A K Biplob <akbiplobx@gmail.com>
2025-08-17 16:13:47 +0600

    d4

---
+++ js/d4.js
@@ -0,0 +1,11 @@
+multiply = function (a, b) {
+  return a * b;
+}
+
+console.log(multiply(4, 5)) //20
+
+// ==============================
+
+ multiply = (a, b) => a * b;
+
+ console.log(multiply(2,3)) //6
