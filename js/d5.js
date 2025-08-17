07148732c96ffd9eee416416cdae5ff9bec6af12
A K Biplob <131751039+akbiplobx@users.noreply.github.com>
2025-08-17 17:43:57 +0600

    "d5"

---
+++ js/d5.js
@@ -0,0 +1,9 @@
+/* you have to 60 tk. if you expense 15 tk , then how many time you can expense?
+& how mauch tk will remaining after expense? write a program */
+
+let ak = 60
+let expense = 15
+let result = ak / expense
+let remaining = ak % expense
+console.log(result) //4
+console.log(remaining) //0
